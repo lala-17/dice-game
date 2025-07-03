5bc7f3bf17473718d2b568a8b512c0e6b6f2f1bb
lala-17 <kirara0851@icloud.com>
2025-07-03 16:38:27 +0900

    Create dice.py
    Dice game : initial revision

---
+++ dice.py
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    char name[100];
+    int die1, die2, total;
+
+    printf("What is your name?\n> ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+
+    srand(time(NULL));
+    printf("Rolling the dice...\n");
+    die1 = rand() % 6 + 1;
+    die2 = rand() % 6 + 1;
+    printf("Die 1: %d\n", die1);
+    printf("Die 2: %d\n", die2);
+
+    total = die1 + die2;
+    printf("Total value: %d\n", total);
+
+    if (total > 7) {
+        printf("%s won!\n", name);
+    } else {
+        printf("%s lost!\n", name);
+    }
+
+    return 0;
+}
